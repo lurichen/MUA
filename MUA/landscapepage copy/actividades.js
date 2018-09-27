e656927208f917cde435c431a7e72b175144357f
luchen <38666985+lurichen@users.noreply.github.com>
2018-09-27 10:23:01 -0700

    Add files via upload

---
+++ MUA/landscapepage copy/actividades.js
@@ -0,0 +1,55 @@
+
+
+/*function createSelect() {
+var myDiv = document.getElementById("myDiv");
+
+  //Create array of options to be added
+  var array = ["Generales - San Francisco", "Generales - Oakland", "Generales - Union City"];
+
+  //Create and append select list
+  var selectList = document.createElement("select");
+  selectList.setAttribute("id", "mySelect");
+  myDiv.appendChild(selectList);
+
+  //Create and append the options
+  for (var i = 0; i < array.length; i++) {
+    var option = document.createElement("option");
+    option.setAttribute("value", array[i]);
+    option.text = array[i];
+    selectList.appendChild(option);
+  }
+}
+document.getElementById("btnTest").addEventListener("click", function(){
+  createSelect();
+});*/
+
+function dropdownValidate() {
+            var result = document.getElementById("mySelect").value;
+            if (result == "0") {
+              alert("Por favor, seleccionar una actividad");
+            else {
+              alert("Seleccionastes : " + result);
+            }
+          }
+
+
+//Assign function name from html
+function myFunction() {
+
+//Take the id from html and let it equal to x
+var x = document.getElementById("myDiv");
+
+//else if statement to hide and show element 
+/*if (x.style.display === "block") {
+    x.style.display = "none";
+} else if (x.style.display === ""){
+  x.style.display = "none";
+}
+  else {
+    x.style.display = "block";
+    }
+  };*/
+
+//
+var y = document.querySelectorAll('.dropbtn');
+target.addEventListener()
---
+++ MUA/landscapepage copy/lulu.js
@@ -0,0 +1,42 @@
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Validate after type ]*/
+    $('.validate-input .input100').each(function(){
+        $(this).on('blur', function(){
+            if(validate(this) == false){
+                showValidate(this);
+            }
+            else {
+                $(this).parent().addClass('true-validate');
+            }
+        })    
+    })
+  
+  
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+           $(this).parent().removeClass('true-validate');
+        });
+    });
