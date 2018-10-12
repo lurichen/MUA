59ff58de145056a87ba5a1e3f851f27a654db3d1
jose d lopez <jose@devlabs.vc>
2018-10-12 10:08:23 -0300

    movimiento de espacio, forma, mas para probar PRs con varios files

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 let data;
 let people = [];
-const addPerson = function(){
-person = document.getElementById("firstNameInp").value;
-people.push(person);
-// lastNameInput = window.getElementById("lasttNameInp");
-if(data = "undefined"){
-	data = window.localStorage;
-}
-data.setItem('Class List', JSON.stringify(people));
+
+const addPerson = function() {
+  person = document.getElementById("firstNameInp").value;
+
+  people.push(person);
+
+  // lastNameInput = window.getElementById("lasttNameInp");
+  if(data = "undefined") {
+	 data = window.localStorage;
+  }
+
+  data.setItem('Class List', JSON.stringify( people ));
 };
+
 
 // C.R.U.D.
 // Create
-
 // Read
- function read(index) { JSON.parse(localStorage.getItem("Class List"))[index]};
+function read(index) { 
+  JSON.parse(localStorage.getItem("Class List"))[index]
+};
 
 // Update
 
